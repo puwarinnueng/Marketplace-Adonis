191db72a0749b7fb957c11ca1ae2ddf2dd8ceea2
puwarinnueng <43135336+puwarinnueng@users.noreply.github.com>
2021-10-16 16:34:02 +0700

    update รูป

--- app/Controllers/Http/CarsController.ts
+++ app/Controllers/Http/CarsController.ts
@@ -21,15 +21,32 @@ export default class CarsController {
   public async cars({ view }) {
     const results = await axios.get('https://carmana.com/api/v2/cars?min_price=&max_price=&min_mileage=&max_mileage=&min_year=&max_year=&is_certified=false&active_year=all&active_price=all&active_mileage=all&page[number]=1&sort=&include=redbook-info.car-submodel.car-model.car-make,car-photos,wished-car,wisher')
     const carsbox = await axios.get('https://carmana.com/api/v2/car-makes')   
-    // console.log(results.data) //array data
-       
+
+    const cars = results.data.data?.map(car => {
+      const photos = car.relationships?.['car-photos']?.data?.map?.(photo => {
+        const photoIncluded =  results.data.included?.find(include => {
+          return include.id == photo.id && include.type == 'car-photos'
+        })
+
+        photo.include = photoIncluded
+        return photo
+      })
+
+      car.photos = photos
+      return car
+    })
+
+    // console.log(cars[0].photos)
+        
     return view.render('pages/cars' , {
-      results: results.data.data,   
-      carsbox: carsbox.data.data  
+      cars: cars,
+      // final:  results.data,     
+      // results: results.data.data,   
+      // results2: results.data.included,   
+      carsbox: carsbox.data.data,
     });
   }
 
-  //รถที่กล่องข้าง
   public async carstypebox({ view }) {
     const carsbox = await axios.get('https://carmana.com/api/v2/car-makes')
        
